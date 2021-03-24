2985694aec2fa9828919736933ee4bbd61399d45
wadimsewo <fungit-hiszoK-3hoxqe>
2021-03-24 21:00:01 +0100

    vue 3 init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router' // <---
+import ElementPlus from 'element-plus';
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import 'element-plus/lib/theme-chalk/index.css';
+import '@/assets/css/tailwind.css'
+
+const app = createApp(App);
+app.use(ElementPlus)
+app.use(router)
+app.use(createPinia())
+app.mount('#app')
